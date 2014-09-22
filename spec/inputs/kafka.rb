577facd7670514673cd1c668aaac07bb990c2d09
Joseph Lawson <joe@joekiller.com>
2014-09-22 13:32:55 -0400

    updated to match logstash changes

--- spec/inputs/kafka.rb
+++ spec/inputs/kafka.rb
@@ -7,17 +7,20 @@ require 'logstash/inputs/kafka'
 require 'logstash/errors'
 
 describe LogStash::Inputs::Kafka do
+  extend LogStash::RSpec
+
+  let (:kafka_config) {{"topic_id" => "test"}}
 
   it 'should populate kafka config with default values' do
-    kafka = LogStash::Inputs::Kafka.new
+    kafka = LogStash::Inputs::Kafka.new(kafka_config)
     insist {kafka.zk_connect} == "localhost:2181"
     insist {kafka.topic_id} == "test"
     insist {kafka.group_id} == "logstash"
     insist {kafka.reset_beginning} == false
   end
 
-  it "should load register and load kafka jars without errors" do
-    kafka = LogStash::Inputs::Kafka.new
+  it "should register and load kafka jars without errors" do
+    kafka = LogStash::Inputs::Kafka.new(kafka_config)
     kafka.register
   end
 
@@ -33,7 +36,7 @@ describe LogStash::Inputs::Kafka do
       end
     end
 
-    kafka = LogStash::Inputs::TestKafka.new
+    kafka = LogStash::Inputs::TestKafka.new(kafka_config)
     kafka.register
 
     class Kafka::Group
@@ -47,6 +50,8 @@ describe LogStash::Inputs::Kafka do
     kafka.run logstash_queue
     e = logstash_queue.pop
     insist { e["message"] } == "Kafka message"
-    insist { e["kafka"] } == {"msg_size"=>13, "topic"=>"test", "consumer_group"=>"logstash"}
+    # no metadata by default
+    insist { e["kafka"] } == nil
   end
+
 end
--- spec/outputs/kafka.rb
+++ spec/outputs/kafka.rb
@@ -3,13 +3,15 @@
 require 'rspec'
 require 'insist'
 require 'logstash/namespace'
-require 'time'
+require "logstash/timestamp"
 require 'logstash/outputs/kafka'
 
 describe LogStash::Outputs::Kafka do
 
+  let (:kafka_config) {{"topic_id" => "test"}}
+
   it 'should populate kafka config with default values' do
-    kafka = LogStash::Outputs::Kafka.new
+    kafka = LogStash::Outputs::Kafka.new(kafka_config)
     insist {kafka.broker_list} == "localhost:9092"
     insist {kafka.topic_id} == "test"
     insist {kafka.compression_codec} == "none"
@@ -18,18 +20,18 @@ describe LogStash::Outputs::Kafka do
     insist {kafka.producer_type} == "sync"
   end
 
-  it "should load register and load kafka jars without errors" do
-    kafka = LogStash::Outputs::Kafka.new
+  it "should register and load kafka jars without errors" do
+    kafka = LogStash::Outputs::Kafka.new(kafka_config)
     kafka.register
   end
 
   it "should send logstash event to kafka broker" do
-    timestamp = Time.now
+    timestamp = LogStash::Timestamp.now
     expect_any_instance_of(Kafka::Producer)
-    .to receive(:sendMsg)
-        .with("test", nil, "{\"message\":\"hello world\",\"host\":\"test\",\"@timestamp\":\"#{timestamp.iso8601(3)}\",\"@version\":\"1\"}")
+    .to receive(:send_msg)
+        .with("test", nil, "{\"message\":\"hello world\",\"host\":\"test\",\"@timestamp\":\"#{timestamp}\",\"@version\":\"1\"}")
     e = LogStash::Event.new({"message" => "hello world", "host" => "test", "@timestamp" => timestamp})
-    kafka = LogStash::Outputs::Kafka.new
+    kafka = LogStash::Outputs::Kafka.new(kafka_config)
     kafka.register
     kafka.receive(e)
   end
